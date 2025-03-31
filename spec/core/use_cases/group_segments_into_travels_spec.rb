ea89f63efd2b897e99a41912359070b7d03362aa
David Martin <11503528+dmartingarcia@users.noreply.github.com>
2025-03-31 14:05:19 +0200

    first draft of a test for the segment grouper

---
+++ spec/core/use_cases/group_segments_into_travels_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require_relative '../../use_cases/group_segments_into_travels'
+require_relative '../../core/entities/flight'
+require_relative '../../core/entities/hotel'
+
+RSpec.describe Core::UseCases::GroupSegmentsIntoTravels do
+  subject(:segment_grouper) { described_class.new(segments, base) }
+
+  let(:base) { 'SVQ' }
+  let(:segments) do
+    [
+      Core::Entities::Flight.new(origin: 'SVQ', destination: 'BCN',
+                                 start_time: DateTime.new(2023, 3, 2, 6, 40),
+                                 end_time: DateTime.new(2023, 3, 2, 9, 10)),
+      Core::Entities::Hotel.new(location: 'BCN',
+                                start_time: DateTime.new(2023, 1, 5, 14),
+                                end_time: DateTime.new(2023, 1, 10, 10)),
+    ]
+  end
+
+  describe '#execute' do
+    it 'groups segments into travels' do
+      travels = segment_grouper.execute
+      expect(travels.size).to eq(1)
+      expect(travels.first.segments.size).to eq(1)
+    end
+  end
+end
