73619f6e6f00f5e8c212c6c1e0d9abdc27099260
David Martin <11503528+dmartingarcia@users.noreply.github.com>
2025-04-01 20:19:41 +0200

    adding explanation for the date and time parsers on the segments entity

--- core/ports/segment_repository_port.rb
+++ core/ports/segment_repository_port.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
+
 require 'time'
 
-# INFO: Any repository that returns segments should inherit from this one, to ensure it has the same interface, it also provides some helper functions
+# INFO: Any repository that returns segments should inherit from this one,
+# to ensure it has the same interface, it also provides some helper functions
 
 module Core
   module Ports
@@ -13,6 +15,7 @@ module Core
       private
 
       def parse_datetime(date_str, time_str)
+        # INFO: Checking the date and time before creating the datetime.
         date = Date.parse(date_str)
         time = Time.parse(time_str)
         DateTime.new(date.year, date.month, date.day, time.hour, time.min)
